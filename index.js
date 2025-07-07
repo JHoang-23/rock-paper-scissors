cc6c869e5ebd196f7b5c6bc0a67f85ac2c4e1c17
John <johnhoang239@gmail.com>
2025-07-07 21:36:29 +0100

    Added round functionality
    Added the logic for determining who wins using
    conditionals.

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-console.log("Hello world!")
+
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3) + 1
     if (choice == 1){
-        return "Rock"
+        return "rock"
     } else if (choice == 2){
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    let userChoice = prompt("Rock, Paper or Scissors?")
+    console.log(userChoice)
+}
+
+getHumanChoice()
+
+function playRound(humanChoice, computerChoice){
+    if (computerChoice == "rock" && humanChoice == "rock"){
+        console.log("Draw! Rock and Rock")
+    } else if (computerChoice == "rock" && humanChoice == "paper"){
+        console.log("You win! Paper beats Rock!")
+    } else if (computerChoice == "rock" && humanChoice == "scissors"){
+        console.log("You lose! Rock beats scissors!")
+    }
+}
+
+const computerChoice = getComputerChoice()
+const humanChoice = getHumanChoice().toLowerCase()
