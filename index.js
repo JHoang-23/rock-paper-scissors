64ee3ff237f466606819871bbfb24c0060c92ae0
John <johnhoang239@gmail.com>
2025-07-09 15:11:35 +0100

    Implement UI and logic changes
    Changed the game to work with buttons which are
    less reliant on user input. Added a simple UI with
    a game log and logic to display the winner.
    Added some simple styling with CSS.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
 let humanScore = 0
 let computerScore = 0
+let gamesPlayed = 0
+
+const outcomes = {
+  rock: { rock: "draw", paper: "lose", scissors: "win" },
+  paper: { rock: "win", paper: "draw", scissors: "lose" },
+  scissors: { rock: "lose", paper: "win", scissors: "draw" }
+}
+
+const messages = {
+  win: "You win! ",
+  lose: "You lose! ",
+  draw: "It's a draw!"
+}
+
+const reasons = {
+  rockpaper: "Paper beats Rock",
+  rockscissors: "Rock beats Scissors",
+  paperrock: "Paper beats Rock",
+  paperscissors: "Scissors beats Paper",
+  scissorsrock: "Rock beats Scissors",
+  scissorspaper: "Scissors beats Paper"
+}
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3) + 1
@@ -13,81 +35,73 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let userChoice = prompt("Rock, Paper or Scissors?")
-    return (userChoice)
-}
-
-
 function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()){
-    console.log(humanChoice)
-    console.log(computerChoice)
-
-
-    if (computerChoice == "rock"){
-        if (humanChoice == "rock"){
-            console.log("It's a draw!")
-        } else if (humanChoice == "paper"){
-            console.log("You win! Paper beats rock")
-            humanScore ++
-        } else{
-            console.log("You lose! Rock beats scissors.")
-            computerScore ++
-        }
-    }
-
-    if (computerChoice == "paper"){
-        if (humanChoice == "rock"){
-            console.log("You lose! Paper beats rock!")
-            computerScore ++
-        } else if (humanChoice == "paper"){
-            console.log("It's a draw!")
-        } else{
-            console.log("You win! Scissors beats paper")
-            humanScore ++
-        }
-    }
-
-    if (computerChoice == "scissors"){
-        if (humanChoice == "rock"){
-            console.log("You win! Rock beats scissors!")
-            humanScore ++
-        } else if (humanChoice == "paper"){
-            console.log("You lose! Scissors beats Paper!")
-            computerScore ++
-        } else{
-            console.log("It's a draw!")
-        }
-    }
-
-
-}
-
-
-
-
-function playGame(){
-    for (let i = 0; i < 5; i++) {
-        playRound()
-        console.log(`The current score is:
-            Computer: ${computerScore} ----- Player ${humanScore}`)
-    }
-
-    console.log(`The final score is:
-        Computer: ${computerScore} ----- Player ${humanScore} `)
-
-    printWinner()
+    const choiceText = document.createElement('p')
+    choiceText.textContent = `
+    You chose: ${humanChoice} ***** CPU chose: ${computerChoice}`
     
+    gameLog.append(choiceText)
+
+    const resultsText = document.createElement('p')
+    const result = outcomes[humanChoice][computerChoice]
+    let key = humanChoice + computerChoice;
+
+    if (result === "win") {
+    humanScore++
+    } else if (result === "lose") {
+    computerScore++
+    }
+
+    const outcomeMessage = messages[result]
+    let reasonMessage = ""
+    if (result !== "draw"){
+        reasonMessage = reasons[humanChoice + computerChoice] //eg rockpaper
+    }
+
+    resultsText.textContent = outcomeMessage + reasonMessage
+
+    gameLog.append(resultsText)
+    displayScore()
+    gamesPlayed++
+
+
 }
 
-playGame()
 
-function printWinner(){
-  if (computerScore > humanScore) {
-    console.log("Computer wins!");
-  } else if (computerScore < humanScore) {
-    console.log("You win!");
-  } else {
-    console.log("It's a draw!");
-  }
+const scoreElement = document.querySelector('.score')
+function displayScore(){
+    console.log(gamesPlayed)
+    const currentScore = document.createElement('h2')
+
+    if (gamesPlayed < 1){ //if first game, create element, change text content and add to score.
+        currentScore.textContent = `Human: ${humanScore} - CPU: ${computerScore}`
+        scoreElement.append(currentScore)
+    } else{
+        const existingScore = document.querySelector('h2')
+        existingScore.textContent = `Human: ${humanScore} - CPU: ${computerScore}`
+
+    }
+
+    const winnerText = document.createElement('p')
+    if (humanScore === 5){
+        winnerText.textContent = "You win!"
+        scoreElement.append(winnerText)
+        btnContainer.style.display = 'none'
+
+    } else if (computerScore === 5){
+        winnerText.textContent = "Computer wins!"
+        scoreElement.append(winnerText)
+        btnContainer.style.display = 'none'
+    }
+
 }
+const btnContainer = document.querySelector(".btn-container")
+console.log(btnContainer)
+btnContainer.addEventListener("click", function(e){
+    console.log(e.target.id)
+
+    playRound(e.target.id)
+})
+
+
+const gameLog = document.querySelector(".game-log")
